0010189b1dd73d476c56f91c725e765ec02eff97
oPablo-Santos <pablo_p9_santos@hotmail.com>
2024-02-12 22:14:55 -0300

    remoção condicional desnecessaria
    Signed-off-by: oPablo-Santos <pablo_p9_santos@hotmail.com>

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -20,12 +20,7 @@ class Users with ChangeNotifier {
   }
 
   void put(User user) {
-    if (user == null) {
-      return;
-    }
-
-    if (user.id != null &&
-        user.id.trim().isNotEmpty &&
+    if (user.id.trim().isNotEmpty &&
         _items.containsKey(user.id)) {
       _items.update(user.id, (_) => user);
     } else {
@@ -43,9 +38,7 @@ class Users with ChangeNotifier {
   }
 
   void remove(User user) {
-    if (user != null && user.id != null) {
-      _items.remove(user.id);
-      notifyListeners();
+    _items.remove(user.id);
+    notifyListeners();
     }
-  }
 }
